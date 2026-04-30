b304d84d152f784b70f217cdaf2970df76325ff0
ShanbelKibre <shambel5110@gmail.com>
2026-04-30 09:28:59 +0300

    change structure by project formate

--- Distributed_System_Data/src/ClientHandler.java
+++ Distributed_System_Data/src/ClientHandler.java
@@ -1,36 +1,4 @@
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
-// import java.io.PrintWriter;
-// import java.net.Socket;
 
-// public // ClientHandler class to handle each client in a separate thread
-// class ClientHandler implements Runnable {
-//    private final Socket clientSocket;
-//    public ClientHandler(Socket socket) {
-//        this.clientSocket = socket;
-//    }
-//    @Override
-//    public void run() {
-//        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-//            String message;
-//            while ((message = in.readLine()) != null) {
-//                System.out.println("Received from client: " + message);
-//                out.println("Echo: " + message); // Echo message back to the client
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                clientSocket.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-// } 
-    
 
 import java.io.*;
 import java.net.*;
